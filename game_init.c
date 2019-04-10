f477e82d1a7c750528f09eca03cc308bbcb4639d
monicaadeyanju <48124262+monicaadeyanju@users.noreply.github.com>
2019-04-10 22:17:17 +0100

    Update game_init.c

--- game_init.c
+++ game_init.c
@@ -1,69 +1,62 @@
-#include "game_init.h"
+#include "game.h"
 #include <stdio.h>
 
-
-/*
-* This function creates the board for the first time
-* Input: board - a 6x9 array of squares.
-*/
+/*  This function creates the board for the first time
+    Input: board - a 6x9 array of squares  */
 void initialize_board(square board[NUM_ROWS][NUM_COLUMNS])
 {
    
-    
-for (int i =0; i< NUM_ROWS; i++)
-{
+    for (int i =0; i< NUM_ROWS; i++)
+    {
         for(int j =0; j < NUM_COLUMNS; j++)
-{
+        {
             //creates an obstacle square at positions (0,3), (1,6), (2,4), (3,5), (4,2) and (5,7)
-if((i == 0 && j==3) || (i == 1 && j == 6) || (i ==2 && j ==4) || (i == 3 && j ==5) || (i==4 && j==2) || (i==5 && j==7))
-{
-board[i][j].type = OBSTACLE;
-} 
-
-else
-{
-//creates a normal square otherwise                
-board[i][j].type = NORMAL;            
-}
-board[i][j].stack = NULL;
-board[i][j].numTokens = 0;
-}    
-}
-
+            if((i == 0 && j==3) || (i == 1 && j == 6) || (i ==2 && j ==4) || (i == 3 && j ==5) || (i==4 && j==2) || (i==5 && j==7))
+            {
+                board[i][j].type = OBSTACLE;
+            } 
+            
+            else
+            {
+                //creates a normal square otherwise
+                board[i][j].type = NORMAL;
+            }
+            
+            board[i][j].stack = NULL;
+            board[i][j].numoftokens = 0;
+        }
+    }
 }
     
     
- 
-/*
-* This function creates players for the first time  
-* Input: the array of players to be initialized
-* Output: The number of players of the game 
-*/
+ /* This function creates players for the first time
+    Input: the array of players to be initialised 
+    Output: The number of players of the game   */
 int initialize_players(player players[])
 {
     int i=1, option;
+    
     while(option!=0)
-    {   
-        printf("Enter the name of the next player.\n");
-        scanf("%s", &players->name[i-1]);
-        printf("Enter 1 to continue or 0 to stop entering names.\n");
-        scanf("%d", &option);
-        
-        if(option==1)
-            i++;
-        
-        //else if(option==0)
-          //  goto END;
+    {
+        printf("Enter name of the player\n");
+        scanf("%s", &players->names[i-1]);
+        printf("Enter 0 to stop inputting names");
+        scanf("%d", option);
+        i++;
         
         if(option!=1)
         {
-            printf("Error.\n");
+            printf("Error");
         }
     }
-    printf("%d\n", i);
-   return i;
+    
+    return i;
+   
 }
+
+
     
    
      
+
 
