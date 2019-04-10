26513772ac05a7d2d87b816c2b78af04f4296825
monicaadeyanju <48124262+monicaadeyanju@users.noreply.github.com>
2019-04-10 22:17:55 +0100

    Update game_init.h

--- game_init.h
+++ game_init.h
@@ -1,75 +1,64 @@
-#ifndef GAME_INIT_H
-#define GAME_INIT_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/*  Monica Adeyanju 
+    Group Project */
 
+/* Defining the columns and rows */
+#ifndef GAME_H
+#define GAME_H
 
-
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* GAME_INIT_H */
-
-//number of rows of the board
 #define NUM_ROWS 6
-
-//number of columns of the board
 #define NUM_COLUMNS 9
 
-//types of squares
-enum stype{
-    NORMAL, OBSTACLE 
+/* Defining the type of squares and tokens */
+enum stype
+{
+    NORMAL, OBSTACLE
 };
 
-
-//colors of tokens
-enum color {
-    RED, BLU, GREEN, YELLOW, PINK, ORANGE
+enum color 
+{
+    RED, BLUE, GREEN, YELLOW, PINK, ORANGE
 };
 
+/* Defining a token and each token is associated with a colour */
+typedef struct token
+{
+   enum color col; 
+    
+}
+token;
 
-//defines a token. 
-
-//Note each token can be associated with a color
-typedef struct token{
-   enum color col;  
-}token;
-
-//Defines a square of the board.
-typedef struct square{
-    //A square can be a NORMAL or an OBSTACLE square
+/* Defining the squares */
+typedef struct square
+{
+    /* A square can be a NORMAL or an OBSTACLE square */
      enum stype type;
-     //the stack of tokens that can be placed on the board square
-     token *stack; 
-     int numTokens;
-}square;
+     /* The stack of tokens that can be placed on the board square */
+     token *stack;  
+     int numoftokens;
+}
+square;
 
+/*  You need to fill this data structure
+    with the information about the player
+    such as a name and a colour. */
+typedef struct player
+{
+    enum color col;
+    char names[20];
+    int numtokenlastcol;
+    
+}
+player; 
 
-
-/*You need to fill this data structure
- * with the information about the player
- * such as a name and a color.*/
-typedef struct player{
-enum color col;    
-char name[20];  
-}player; 
-
-
-/*This function creates the board for the first time
- * Input: board - a 6x9 array of squares*/
+/*  This function creates the board for the first time
+    Input: board - a 6x9 array of squares   */
 void initialize_board(square board[NUM_ROWS][NUM_COLUMNS]);
-
-/*This function creates players for the first time 
- * Input: the array of players to be initialized
- * Output: The number of players of the game*/
+    
+/* This function creates players for the first time
+   Input: the array of players to be initialised
+   Output: The number of players of the game */
 int initialize_players(player players[]);
 
-
-
-
-
+#endif	// GAME_H
 
