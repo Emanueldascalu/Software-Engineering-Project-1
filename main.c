7cfa9a6a16fad73e7f8da84dfd6719b5e85aa995
monicaadeyanju <48124262+monicaadeyanju@users.noreply.github.com>
2019-04-15 14:50:53 +0100

    Update main.c

--- main.c
+++ main.c
@@ -22,10 +22,10 @@ int main(int argc, char** argv)
     numPlayers = initialize_players(players);
     
     /* asks each player to place their tokens on the first column of the board */
-    place_tokens(board, players, numPlayers);
+    place_tokens(board, players, numPlayers, topToken);
     
     /* manages the turns of the game and identifies a winner */
-    play_game(board, players, numPlayers);
+    play_game(board, players, numPlayers, topToken);
     
     return 0;
 }
