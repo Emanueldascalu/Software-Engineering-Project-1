2510d5e5aba715ec7111f145d90b767e0f146b18
Emanueldascalu <48124221+Emanueldascalu@users.noreply.github.com>
2019-04-17 02:17:21 +0100

    Update game_logic.h

--- game_logic.h
+++ game_logic.h
@@ -1,5 +1,3 @@
-/*  Prints the board
-    Input: the board to be printed. */
 #ifndef GAMELOGIC_H
 #define GAMELOGIC_H
 
@@ -9,15 +7,17 @@ void print_board(square board[NUM_ROWS][NUM_COLUMNS]);
     Input: board - a 6x9 array of squares that represents the board
     players - the array of the players
     numPlayers - the number of players  */
-void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers,token *top);
+void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers);
 
 /* Manages the logic of the game 
     Input: board - a 6x9 array of squares that represents the board
     players - the array of the players
     numPlayers - the number of players */
-void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers, token *top);
+void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers);
 
 
+token * addTokens(enum color col, token * top);
+
+token * removeTokens(token * top);
 
 #endif	// GAMELOGIC_H
-
