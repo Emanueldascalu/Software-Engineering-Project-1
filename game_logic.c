b93490c5373c853a83f5e5e9c017ab8f43df2de5
Emanueldascalu <48124221+Emanueldascalu@users.noreply.github.com>
2019-04-21 17:37:08 +0100

    Update game_logic.c

--- game_logic.c
+++ game_logic.c
@@ -97,7 +97,6 @@ void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int num
                 {
                     minimumTokens++;
                 }
- 
             }
             
             else if(board[selectedSquare][0].numoftokens != minimumTokens || board[selectedSquare][0].stack->col == players[j].col)
@@ -125,98 +124,193 @@ void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPla
 {
     srand(time(NULL));
     
-    int choice, choice1, i, j, x, k;
+    int choice, choice1, i, j;
     int dice;
     int row, column;
-   
-    while(players->numtokenlastcol != 3)   
+    while(players->numtokenlastcol != 3) 
     {
-        START:
         for(i=1;i<=numPlayers;i++)
         {
-            printf("Player %d input: ", i); 
-            printf("1 if you want to move one of your tokens up or down.\nOtherwise, Input 2 to go straight to sideways movement\n");
+            dice = rand()%6;
+            printf("\nRolling the dice.\nDice = %d.\n\n", dice);
+            printf("Player %d input:\n", i); 
+            printf("1 if you want to move one of your tokens up or down.\nOtherwise, chose 2 to move a token forward.\n");
+            
             scanf("%d",&choice);
-           
-            if(choice==1)
+            if(choice == 1)//switch(choice)
             {
+                START:
                 printf("Enter the row your token is located on.\n");
                 scanf("%d", &row);
                 printf("Enter the column your token is located on.\n");
                 scanf("%d", &column);
+                if(board[row][column].stack == NULL)
+                {
+                    printf("There is no token placed here.\nTry again.\n"); 
+                    goto START;
+                }
                 printf("Enter 1 to move up or 2 to move down.\n");
                 scanf("%d", &choice1);
                 
-                if(choice1 == 1 && (row < 6 && row >= 1) && (column < 9 && column >= 0) && (board[row][column].stack->col == players[i-1].col))
-                    {
-                        board[row-1][column].stack = addTokens(players[i-1].col, board[row-1][column].stack);
-                        board[row][column].stack = removeTokens(board[row][column].stack);
-                        print_board(board);
-                        
-                    }
                 
-                else if (choice1 == 2 && (row < 5 && row >= 0) && (column < 9 && column >= 0) && (board[row][column].stack->col == players[i-1].col))
+                    
+                if(choice1 == 1 && (row < 6 && row >= 1) && (column < 9 && column >= 0) && (board[row][column].stack->col == players[i-1].col))
+                {
+                    if((row==0 && column==3) || (row==1 && column==6) || (row==2 && column==4) || (row==3 && column==5) || (row==4 && column==2) || (row==5 && column==7))
                     {
-                        board[row+1][column].stack = addTokens(players[i-1].col, board[row+1][column].stack);
+                        int numNULL = 0;
+                        for(int k=0; k<column; k++)
+                        {   for(int l=0; l<NUM_ROWS; l++)
+                            {
+                                if(board[l][k].stack == NULL)
+                                ++numNULL;
+                            }
+                        }
+
+                        if(numNULL == NUM_ROWS * column)
+                        {
+                            printf("There are no tokens behind your chosen token.\n It can now be moved.\n\n");
+                            board[row-1][column].stack = addTokens(board[row][column].stack->col, board[row-1][column].stack);
+                            board[row][column].stack = removeTokens(board[row][column].stack);
+                            print_board(board);
+                            goto END;
+                        }
+
+                        else
+                            printf("This token can't be moved as it's on an obstacle square.\n\n");
+                    }
+
+                    else
+                    {
+                        board[row-1][column].stack = addTokens(board[row][column].stack->col, board[row-1][column].stack);
                         board[row][column].stack = removeTokens(board[row][column].stack);
                         print_board(board);
-                        
-                    }     
+                        goto END;
+                    }
+
+                }
+
+                else if (choice1 == 2 && (row < 5 && row >= 0) && (column < 9 && column >= 0) && (board[row][column].stack->col == players[i-1].col))
+                {
+                    if((row==0 && column==3) || (row==1 && column==6) || (row==2 && column==4) || (row==3 && column==5) || (row==4 && column==2) || (row==5 && column==7))
+                    {
+                        int numNULL = 0;
+                        for(int k=0; k<column; k++)
+                        {   for(int l=0; l<NUM_ROWS; l++)
+                            {
+                                if(board[l][k].stack == NULL)
+                                ++numNULL;
+                            }
+                        }
+
+                        if(numNULL == NUM_ROWS * column)
+                        {
+                            printf("There are no tokens behind your chosen token.\n It can now be moved.\n\n");
+                            board[row+1][column].stack = addTokens(board[row][column].stack->col, board[row+1][column].stack);
+                            board[row][column].stack = removeTokens(board[row][column].stack);
+                            print_board(board);
+                            goto END;
+                        }
+
+                        else
+                            printf("This token can't be moved as it's on an obstacle square.\n\n");
+                    }
+
+                    else
+                    {
+                        board[row+1][column].stack = addTokens(board[row][column].stack->col, board[row+1][column].stack);
+                        board[row][column].stack = removeTokens(board[row][column].stack);
+                        print_board(board);
+                        goto END;
+                    }
+
+                }
+
+                else
+                {
+                    printf("One or more of your inputs was invalid. Try again.\n");
+                    i--;
+                }
             }
-            
-            dice = rand()%5;
-            printf("\nRolling the dice.\nDice = %d.\n\n", dice);
-            printf("Move forward a token on [%d].\n", dice);
-            printf("If there are no tokens to move, Input 1. Otherwise input 2\n");
-            scanf("%d", &x);
-                    
-            if(x!=1)
-            {
+                    //break;
+            else if(choice == 2)
+            {   
+                END:
                 printf("Enter the column in which your chosen token is located.\n");
                 scanf("%d", &column);
-                        
-                if(column < 8 && column >=0 )
+                
+                if(board[dice][column].stack == NULL)
+                {
+                    printf("There is no token placed here.\nTry again.\n"); 
+                    goto END;
+                }
+                
+                else if(column < 8 && column >=0 )
                 {
                     if((dice==0 && column==3) || (dice==1 && column==6) || (dice==2 && column==4) || (dice==3 && column==5) || (dice==4 && column==2) || (dice==5 && column==7))
                     {
-                        for (j =0; j<NUM_ROWS;j++)
-                        {
-                            for(k=0;k<column;k++)
+                        int numNULL = 0;
+                        for(int k=0; k<column; k++)
+                        {   for(int l=0; l<NUM_ROWS; l++)
                             {
-                                if(board[j][k].stack != NULL)
-                                {
-                                    printf("\nToken is Stuck\n\n");
-                                    goto START;
-                                }
-                                
+                                if(board[l][k].stack == NULL)
+                                ++numNULL;
                             }
-                        
-                        }   
+                        }
+
+                        if(numNULL == NUM_ROWS * column)
+                        {
+                            printf("There are no tokens behind your chosen token.\n It can now be moved.\n\n");
+                            board[dice][column+1].stack = addTokens(board[dice][column].stack->col, board[dice][column+1].stack);
+                            if(column == 7)
+                            {
+                                for (int m=1; m<numPlayers; m++)
+                                {
+                                    if(players[m-1].col == board[dice][column].stack->col)
+                                    {
+                                        ++players[m-1].numtokenlastcol;
+                                        if(players[m-1].numtokenlastcol == 3)
+                                            goto WIN;
+                                    }
+                                }
+                            }
+                            board[dice][column].stack = removeTokens(board[dice][column].stack);
+                            print_board(board);
+                        }
+                        else
+                            printf("This token can't be moved as it's on an obstacle square.\n\n");    
                     }
-                    
+
                     else
                     {
-                        board[dice][column+1].stack = addTokens(players[i-1].col, board[dice][column+1].stack);
-                        board[dice][column].stack = removeTokens(board[dice][column].stack);
-                        print_board(board);
-                        
-                        if(column==7)
+                        board[dice][column+1].stack = addTokens(board[dice][column].stack->col, board[dice][column+1].stack);
+                        if(column == 7)
                         {
-                            players[i-1].numtokenlastcol++;
+                            for (int m=1; m<numPlayers; m++)
+                            {
+                                if(players[m-1].col == board[dice][column].stack->col)
+                                {
+                                    ++players[m-1].numtokenlastcol;
+                                    if(players[m-1].numtokenlastcol == 3)
+                                        goto WIN;
+                                }
+                            }
                         }
+                        board[dice][column].stack = removeTokens(board[dice][column].stack);
+                        print_board(board);  
                     }
                 }
             }
             
             else
-            {
-                i--;
-            }
-        
+                {
+                    printf("One or more of your inputs were invalid. Try again.\n");
+                    i--;   
+                } 
         }
     }
-    
-    printf("Winner is Player %d\n", i-1);
+    WIN:
+    printf("The winner is player %d or %s", i, players[i-1].names);
 }
 
 token * addTokens(enum color col, token * top)
