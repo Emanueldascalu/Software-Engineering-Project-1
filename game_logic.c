0ea56a0fa74eee8a6e74b55c1566c406d74a02f1
monicaadeyanju <48124262+monicaadeyanju@users.noreply.github.com>
2019-04-22 22:14:46 +0100

    Update game_logic.c

--- game_logic.c
+++ game_logic.c
@@ -80,6 +80,8 @@ void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int num
     int selectedSquare = 0;
     int i, j;
     
+    /* Allowing each player to place their four tokens on a square
+       selected by the user */
     for(i=0;i<4;i++)   
     {
         for(j=0;j<numPlayers;j++)
@@ -87,8 +89,11 @@ void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int num
             printf("Player %d, please select a square\n", (j+1));
             scanf("%d", &selectedSquare);
             
+            /* We made sure all the square in the first column are populated 
+               before allowing the stacking of tokens */
             if(board[selectedSquare][0].numoftokens == minimumTokens && board[selectedSquare][0].stack == NULL)
             {
+                /* the function addTokens is called to add the player's token to the selected square */
                 board[selectedSquare][0].stack = addTokens(players[j].col, board[selectedSquare][0].stack);
                 board[selectedSquare][0].numoftokens++;
                 print_board(board);
@@ -104,7 +109,9 @@ void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int num
                 printf("Error\n");
                 j--;
             }
-                
+            
+            /* This allows the stacking of tokens once all the square have been
+               covered in the first column */
             else if(board[selectedSquare][0].numoftokens == minimumTokens && board[selectedSquare][0].stack != NULL) 
             {
                 board[selectedSquare][0].stack = addTokens(players[j].col, board[selectedSquare][0].stack);
@@ -127,23 +134,27 @@ void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPla
     int choice, choice1, i, j;
     int dice;
     int row, column;
+    
+    /* The game runs until a winner is found */
     while(players->numtokenlastcol != 3) 
     {
         for(i=1;i<=numPlayers;i++)
         {
-            dice = rand()%6;
-            printf("\nRolling the dice.\nDice = %d.\n\n", dice);
+            /* Asking for the players how they want to move their tokens */
             printf("Player %d input:\n", i); 
             printf("1 if you want to move one of your tokens up or down.\nOtherwise, chose 2 to move a token forward.\n");
             
             scanf("%d",&choice);
-            if(choice == 1)//switch(choice)
+            if(choice == 1)
             {
                 START:
+                /* Asking for the row and column of the token they want to move vertically */
                 printf("Enter the row your token is located on.\n");
                 scanf("%d", &row);
                 printf("Enter the column your token is located on.\n");
                 scanf("%d", &column);
+                
+                /* If there is no token on the square, the player gets to input another square */
                 if(board[row][column].stack == NULL)
                 {
                     printf("There is no token placed here.\nTry again.\n"); 
@@ -152,12 +163,17 @@ void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPla
                 printf("Enter 1 to move up or 2 to move down.\n");
                 scanf("%d", &choice1);
                 
-                
-                    
+                /* We are making sure the input by the player fits into the game's
+                   criteria and it has their token on the top of the stack before
+                   they move their token upwards */
                 if(choice1 == 1 && (row < 6 && row >= 1) && (column < 9 && column >= 0) && (board[row][column].stack->col == players[i-1].col))
                 {
+                    /* We are checking if the token is on an obstacle square and restricting
+                       the player from moving the token unless all there are no token behind 
+                       it  */
                     if((row==0 && column==3) || (row==1 && column==6) || (row==2 && column==4) || (row==3 && column==5) || (row==4 && column==2) || (row==5 && column==7))
                     {
+                        /* Going through the squares behind the token to check if there are Null */
                         int numNULL = 0;
                         for(int k=0; k<column; k++)
                         {   for(int l=0; l<NUM_ROWS; l++)
@@ -167,6 +183,8 @@ void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPla
                             }
                         }
 
+                        /* If there are no tokens behind the selected token, it can be moved 
+                           to another square and the new board is printed */
                         if(numNULL == NUM_ROWS * column)
                         {
                             printf("There are no tokens behind your chosen token.\n It can now be moved.\n\n");
@@ -180,6 +198,8 @@ void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPla
                             printf("This token can't be moved as it's on an obstacle square.\n\n");
                     }
 
+                    /* If the token is not on an obstacle, it automatically allows the player
+                       to move their token */
                     else
                     {
                         board[row-1][column].stack = addTokens(board[row][column].stack->col, board[row-1][column].stack);
@@ -190,6 +210,7 @@ void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPla
 
                 }
 
+                /* The same method is implemented if a player wants to move their token downwards */
                 else if (choice1 == 2 && (row < 5 && row >= 0) && (column < 9 && column >= 0) && (board[row][column].stack->col == players[i-1].col))
                 {
                     if((row==0 && column==3) || (row==1 && column==6) || (row==2 && column==4) || (row==3 && column==5) || (row==4 && column==2) || (row==5 && column==7))
@@ -226,27 +247,51 @@ void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPla
 
                 }
 
+                /* If a player inputs the wrong details, i is 
+                   decremented to give them another try */
                 else
                 {
                     printf("One or more of your inputs was invalid. Try again.\n");
                     i--;
                 }
             }
-                    //break;
+            
+            /* Players have to move a token forward and they can move other 
+               player's token not only theirs */
             else if(choice == 2)
             {   
+                int n;
+                
                 END:
+                dice = rand()%6;
+                printf("\nRolling the dice.\nDice = %d.\n\n", dice);
+                printf("If there are no tokens on this row, input 1 to reroll the dice else input 2.\n\n");
+                scanf("%d", &n);
+                
+                if(n==1)
+                {
+                    goto END;
+                }
+                
                 printf("Enter the column in which your chosen token is located.\n");
                 scanf("%d", &column);
                 
+                /* The dice is used as the row for each player and they are 
+                   allowed to input their own column. The square selected is
+                   checked to make sure it is not empty */
                 if(board[dice][column].stack == NULL)
                 {
                     printf("There is no token placed here.\nTry again.\n"); 
                     goto END;
                 }
                 
+                /* If it is not empty, the game's criteria are checked before
+                   the token can be moved */
                 else if(column < 8 && column >=0 )
                 {
+                    /* If the obstacle is on an obstacle, it is once again checked to 
+                       make sure there are no tokens behind it, if so it can be moved 
+                       forward */
                     if((dice==0 && column==3) || (dice==1 && column==6) || (dice==2 && column==4) || (dice==3 && column==5) || (dice==4 && column==2) || (dice==5 && column==7))
                     {
                         int numNULL = 0;
@@ -262,10 +307,18 @@ void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPla
                         {
                             printf("There are no tokens behind your chosen token.\n It can now be moved.\n\n");
                             board[dice][column+1].stack = addTokens(board[dice][column].stack->col, board[dice][column+1].stack);
+                            
+                            /* If a player selects column 7 when moving a token 
+                               forward, it is going onto the last column which
+                               should increase the player who owns that token, number
+                               of tokens on the last column */
                             if(column == 7)
                             {
                                 for (int m=1; m<numPlayers; m++)
                                 {
+                                    /* We go through the players to make sure the right player's
+                                       tokens on the last column are incremented and if it is
+                                       equivalent to 3 they win */
                                     if(players[m-1].col == board[dice][column].stack->col)
                                     {
                                         ++players[m-1].numtokenlastcol;
@@ -281,6 +334,8 @@ void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPla
                             printf("This token can't be moved as it's on an obstacle square.\n\n");    
                     }
 
+                    /* If there are no tokens behind the selected token, it can be 
+                       moved forward to another square and the new board is printed */
                     else
                     {
                         board[dice][column+1].stack = addTokens(board[dice][column].stack->col, board[dice][column+1].stack);
@@ -310,11 +365,14 @@ void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPla
         }
     }
     WIN:
+    /* The winner is printed */
     printf("The winner is player %d or %s", i, players[i-1].names);
 }
 
 token * addTokens(enum color col, token * top)
 {
+    /* This uses malloc to create memory for the
+       stack of tokens */
     token * current = top;
     top = (token*)malloc(sizeof(token));
     top->col = col;
@@ -323,6 +381,8 @@ token * addTokens(enum color col, token * top)
 }
 token * removeTokens(token * top)
 {
+    /* This function checks the top token
+       and removes it from the stack */
     token * current = top;
     if(current != NULL)
     {
